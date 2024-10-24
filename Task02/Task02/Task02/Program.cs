9950f22736bc3d2c440780c9f903285779120a97
Giorgi Mebonia <giorgimebonia@credo.ge>
2024-10-24 12:39:44 +0400

    my personal info

---
+++ Task02/Task02/Task02/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task02
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello, World");
+
+
+            //short -16,
+            //int -32,
+            //long -64,
+            //byte -8, 
+
+            string firstName = "Giorgi";
+            string lastName = "Mebonia";
+            string fullName = firstName + lastName;
+            short age = 38;
+            float height = 1.86f;
+            double weigght = 103.5;
+            bool isMarried = true;
+            bool isMale = true;
+            bool hasChild = true;
+            string jobTitle = "Credo JSC";
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(fullName);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(weigght);
+            Console.WriteLine(isMarried);
+            Console.WriteLine(isMale);
+            Console.WriteLine(hasChild);
+            Console.WriteLine(jobTitle);
+        }
+    }
+}
+
+
+
