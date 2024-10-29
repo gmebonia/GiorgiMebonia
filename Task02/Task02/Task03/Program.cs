8f0fd9facad39629dd16402d94a1802760c41df7
Giorgi Mebonia <giorgimebonia@credo.ge>
2024-10-29 17:23:48 +0400

    Guess random number

--- Task02/Task02/Task03/Program.cs
+++ Task02/Task02/Task03/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+    Random random = new Random();
+    int randomNumber = random.Next(1, 10);
+    Console.WriteLine(randomNumber);
+
+
+    Console.WriteLine("Gamoicanit Ricxvi  1-dan 10-is chatvlit, 3 cda:");
+
+// First Try
+
+ int number1 = int.Parse(Console.ReadLine());
+    if (randomNumber != number1)
+{
+    Console.WriteLine("KIDEV SCADE");
+   
+}
+else
+{
+    Console.WriteLine("CIFRI GAMOICANIT");
+    return;
+}
+
+// Second Try
+
+int number2 = int.Parse(Console.ReadLine());
+if (randomNumber != number2)
+{
+    Console.WriteLine("KIDEV SCADE");
+
+}
+else
+{
+    Console.WriteLine("CIFRI GAMOICANIT");
+    return;
+}
+
+// Third Try
+
+int number3 = int.Parse(Console.ReadLine());
+if (randomNumber != number3)
+{
+    Console.WriteLine("KIDEV SCADE");
+
+}
+else
+{
+    Console.WriteLine("CIFRI GAMOICANIT");
+    return;
+}
+
