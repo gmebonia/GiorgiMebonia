4587a0ae3d3aea4bee3ac387185e5d92912bc750
Giorgi Mebonia <giorgimebonia@credo.ge>
2024-10-31 23:35:09 +0400

    metodebi

---
+++ Tasks/All Tasks/Task06/Task06/Program.cs
@@ -0,0 +1,23 @@
+﻿//metodebi
+
+int index = 0;
+Console.WriteLine("Enter index:");
+index = int.Parse(Console.ReadLine());
+int[] array = new int[5];
+Console.WriteLine("Enter an integer:");
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Result:" + sumNumber(index, array));
+static int sumNumber(int index, int[] array)
+{
+    int num = array[index - 1];
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
