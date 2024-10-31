aab85ffdc8048c169d5ce34ddaef11a4986935d9
Giorgi Mebonia <giorgimebonia@credo.ge>
2024-10-31 22:05:59 +0400

    dadebiti ricxvebi

---
+++ Tasks/All Tasks/Task04-2/Task04-2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//dadebiti ricxvebi
+
+
+int Number = 0;
+int Sum = 0;
+string EnteredNumber = "";
+
+while (Number >= 0)
+{
+    Console.WriteLine("Enter Number");
+    Number = int.Parse(Console.ReadLine());
+    if (Number > 0)
+    {
+
+        Sum = Sum + Number;
+
+    }
+
+    EnteredNumber += Number + ";";
+
+}
+
+Console.WriteLine("Enter Numbers: " + EnteredNumber);
+Console.WriteLine("Sum:" + Sum);
