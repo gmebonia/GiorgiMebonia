e3410c162d76a7911128a2f633a8a5a5c65d9ca7
Giorgi Mebonia <giorgimebonia@credo.ge>
2024-11-08 08:51:02 +0400

    stack da heap personis klasis Seqmna cvlladebiT

---
+++ Tasks/All Tasks/Task08/Task08/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task08
+{
+    public class Person //პერსონის კლასის შექმნა სახელი და ასაკი
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Tasks/All Tasks/Task08/Task08/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Task08;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //ორი  ცვლადის შექმნა და მნიშვნელობის მინიჭება
+
+        string name = "Mebo";
+        int age = 38;
+
+        // ობიექტის შექმნა
+
+        Person person1 = new Person(name, age);
+
+        //ახალი ობიექტის შექმნა და შენახვა
+
+        Person person2 = person1;
+
+        //ობიექტზე ველების მნიშვნელობების შეცვლა
+
+        person2.Age = 30;
+        person2.Name = "Salome";
+
+
+        //რომ ვნახოთ სად რა ხდება
+
+        Console.WriteLine($"name: {name}");
+        Console.WriteLine($"age: {age}");
+        Console.WriteLine($"person1Name: {person1.Name}");
+        Console.WriteLine($"person1Age: {person1.Age}");
+        Console.WriteLine($"person2Name: {person2.Name}");
+        Console.WriteLine($"person2Age: {person2.Age}");
+
+
+    }
+
+}
