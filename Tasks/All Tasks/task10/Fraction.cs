1b048086c06ee8f137764898be53b56b9dfec056
Giorgi Mebonia <giorgimebonia@credo.ge>
2024-11-28 15:15:52 +0400

    oop fraction task

---
+++ Tasks/All Tasks/task10/Fraction.cs
@@ -0,0 +1,67 @@
+﻿
+namespace task10
+{
+    public class Fraction
+    {
+        private int _numerator; // მრიცხველი
+        private int _denominator; //მნიშვნელი
+
+        public Fraction(int numerator, int denominator) //კონსტრუქტორის მომზადება
+        {
+            if (denominator == 0)
+                throw new DivideByZeroException();
+
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+
+        public override string ToString()  //თუ სტრინგ მეთოდის გადატვირთვა
+
+        {
+            return $"{_numerator}/{_denominator}";
+        }
+
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2) // მიმატების ოპერატორის გადატვირთვა
+        {
+            if (fraction1._denominator == fraction2._denominator)
+            {
+                return new Fraction(fraction1._numerator + fraction2._numerator, fraction1._denominator);
+            }
+
+            else
+            {
+                int denominator = LCM(fraction1._denominator, fraction2._denominator);
+                int numerator = (denominator / fraction1._denominator * fraction1._numerator) + (denominator / fraction2._denominator * fraction2._numerator);
+                int gcd = GCD(numerator, denominator);
+                return new Fraction(numerator / gcd, denominator / gcd);
+
+            }
+        }
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private static int LCM(int a, int b)
+        {
+            return (a * b) / GCD(a, b);
+        }
+
+        public static bool operator ==(Fraction fraction1, Fraction fraction2) //შედარების ოპერატორის გადატვირთვა
+        {
+            return (fraction1._numerator == fraction2._numerator && fraction1._denominator == fraction2._denominator);
+        }
+
+        public static bool operator !=(Fraction fraction1, Fraction fraction2)
+        {
+            return !(fraction1 == fraction2);
+        }
+    }
+
+}
---
+++ Tasks/All Tasks/task10/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fraction1 = new Fraction(3, 5); //აქ თუ მივუთითებთ ნულს მაშინ ისვრის ექსეფშენს
+            var fraction2 = new Fraction(1, 2);
+            Console.WriteLine(fraction1.ToString());
+            Console.WriteLine(fraction2.ToString());
+
+            var sumOfFractions = fraction1 + fraction2; //წილადების მიმატება
+            Console.WriteLine(sumOfFractions.ToString());
+
+            if (fraction1 == fraction2) //ტოლია თუ არა წილადები ერთმანეთთან
+            {
+                Console.WriteLine("pirveli = meores");
+            }
+
+            if (fraction1 != fraction2)
+            {
+                Console.WriteLine("pirveli ar udris meores");
+            }
+        }
+
+    }
+}
